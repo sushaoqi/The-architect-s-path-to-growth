1887a3a72bbbaba32be1fb58c244ae9855ecb9b3
sushaoqi <2257346739@qq.com>
2019-05-22 11:01:21 +0800

    懒汉式  类初始化的,不会创建该对象,真正需要的时候才会加载(创建),天生线程不安全,需要解决线程安全问题,所以效率非常低

--- calss_design_model/src/main/java/com/juzss/model/Singleton.java
+++ calss_design_model/src/main/java/com/juzss/model/Singleton.java
@@ -5,7 +5,7 @@ package com.juzss.model;
 public class Singleton {
   
 	
-	// 类初始化的时候,就会创建对象,天生线程安全,调用效率比较高,如果在不使用对象的时候,会浪费内存
+	// 类初始化的时候,就会创建对象,天生线程安全,调用效率比较高,如果在不使用对象的时候,会浪费内存。
 	private static final Singleton singleton = new Singleton();
 	//将构造函数私有化
 	private Singleton() {}
